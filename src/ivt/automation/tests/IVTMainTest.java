f0d46d05b09ae29396561cb584340c007b97332e
Charmani <094539@NTTDATA.COM>
2020-09-22 17:48:41 +0530

    Changes related to TUKPAPREFEE, TUKMONTHLYEXTRA_TOTAL, TUKAIRTIMEPLANTOTAL

--- src/ivt/automation/tests/IVTMainTest.java
+++ src/ivt/automation/tests/IVTMainTest.java
@@ -1,18 +1,11 @@
 package ivt.automation.tests;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.commons.lang3.StringUtils;
 
-import ivt.automation.businessrules.Adj;
 import ivt.automation.businessrules.IVTSingleTagCompareFiles;
 import ivt.automation.businessrules.TukAirTimePlanTotal;
-import ivt.automation.businessrules.TukCCATotal;
-import ivt.automation.businessrules.TukCurrentSpendCap;
-import ivt.automation.businessrules.TukDiscountLineTotal;
 import ivt.automation.businessrules.TukLateFee;
-import ivt.automation.businessrules.TukPaperFee;
+import ivt.automation.businessrules.TukPaperFeeNew;
 import ivt.automation.core.IVTBase;
 
 public class IVTMainTest {
@@ -27,38 +20,12 @@ public class IVTMainTest {
 			String b[] = IBM.split("\\\\");
 			int len = b.length;
 			IVTBase.ACCOUNTNUMBER = StringUtils.substringAfter(b[len-1], "_");
-			System.out.println(IVTBase.ACCOUNTNUMBER);
+			//System.out.println(IVTBase.ACCOUNTNUMBER);
 			
-			IVTSingleTagCompareFiles.compareIBMAndNCSingleTags(IBM, NC);
-			Adj.CompareADJTags(IBM,NC);
-			TukPaperFee.compareTukPaperFee(IBM, NC);
-			TukLateFee.compareTukLateFee(IBM, NC);
-			TukCurrentSpendCap.compareSpendCap(IBM, NC);
+			//IVTSingleTagCompareFiles.compareIBMAndNCSingleTags(IBM, NC);
+			TukPaperFeeNew.compareTukPaperFee(IBM, NC);	
+			//TukLateFee.compareTukLateFee(IBM, NC);
 			TukAirTimePlanTotal.compareAirTimePlanTotal(IBM,NC);
-			TukDiscountLineTotal.compareDiscountLineTotal(IBM,NC);
-		}
-		
-		for (String str : IVTBase.fetchCCAAndNCFiles()) {
-			List<String> ccalist = new ArrayList<>(); 
-			
-			String a[] = str.split("\\|");
-			NC = a[0];
-			
-			String b[] = NC.split("\\\\");
-			int len = b.length;
-			IVTBase.ACCOUNTNUMBER = StringUtils.substringAfter(b[len-1], "_");
-			System.out.println(IVTBase.ACCOUNTNUMBER);
-			
-			if(a.length==2) {
-				CCA = a[1];
-				TukCCATotal.compareTukCCATotal(CCA, NC);	
-			}
-			else {
-				for(int i=1;i<a.length;i++) {
-					ccalist.add(a[i]);
-				}
-				TukCCATotal.compareTukCCAListTotal(ccalist, NC);
-			}
 		}
 	}
 }
